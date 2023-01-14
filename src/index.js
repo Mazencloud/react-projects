2ba334d7ce2e4ca7a96c78e9c95362524f7721bf
Mazen Elhamy <78644998+Mazencloud@users.noreply.github.com>
2023-01-14 15:58:54 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import Game from "./components/Game";
 
-ReactDOM.render(
-
-    <Router>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/Register' element={<Register />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Cart" element={<Cart />} />
-      </Routes>
-    </Router>,
-
-  document.getElementById('root')
-);
+ReactDom.render(<Game />, document.getElementById("root"));
