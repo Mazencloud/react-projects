57b8ee76afb2f4d326b36917ae63b3597921421d
Mazen Elhamy <78644998+Mazencloud@users.noreply.github.com>
2023-01-18 16:43:19 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import Home from "./pages/home";
+import React, { useState, useEffect } from 'react';
+import './App.css'
+import Form from './components/Form';
+import Header from './components/Header';
+import TodosList from './components/TodosList';
+
 
 const App = () => {
-  return <Home />;
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <Header />
+        <Form input={input} setInput={setInput} todos={todos} setTodos={setTodos} editTodo={editTodo} setEditTodo={setEditTodo} />
+        <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Cart from "./pages/Cart";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 ReactDOM.render(
 
-    <Router>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/Register' element={<Register />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Cart" element={<Cart />} />
-      </Routes>
-    </Router>,
-
+    <App />,
   document.getElementById('root')
 );
